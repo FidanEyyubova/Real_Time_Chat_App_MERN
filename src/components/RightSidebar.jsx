84bb5f238e4974dfd4c5afc838263377afff165a
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-08-09 18:19:24 +0400

    Start Right Sidebar

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const RightSidebar = () => {
-  return (
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
     <div>
-      <h1>Right Sidebar</h1>
+      <div>
+        <img src={selectedUser?.profilePic} alt="" className='w-20 aspect-[1/1] rounded-full' />
+      <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
+        <p className=''></p>
+        {selectedUser.name}
+      </h1>
+      </div>
     </div>
   )
 }
